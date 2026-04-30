82bb9e7839c83fd7e999bd69f71c496962e2a429
Jason Fopiano <144917581+JFopiano@users.noreply.github.com>
2026-04-30 18:14:42 -0400

    Player Movement Added
    We can now walk around with keyboard controls... yippeee

---
+++ Assets/Scripts/MazePlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MazePlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3.5f;
+    public float mouseSensitivity = 2f;
+
+    public Transform playerCamera;
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // For testing and debugging
+        Debug.Log("Player script is running");
+        if (Input.GetKey(KeyCode.W))
+            Debug.Log("W key is pressed");
+
+
+
+
+        // WASD movement, same style as your cannon project
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        // mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CSC493-Final-Project
-Randomly generate a maze with an enemy chasing you before you make your way out
