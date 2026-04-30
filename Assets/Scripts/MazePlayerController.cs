db1d33cfbf6e2f77a1cf24dfe5ce0d93195f2ac0
Jason Fopiano <144917581+JFopiano@users.noreply.github.com>
2026-04-30 18:58:30 -0400

    Collisions Added
    Fixed issue of player able to walk through the maze walls. Added collider to fix this issue

--- Assets/Scripts/MazePlayerController.cs
+++ Assets/Scripts/MazePlayerController.cs
@@ -4,41 +4,51 @@ public class MazePlayerController : MonoBehaviour
 {
     public float moveSpeed = 3.5f;
     public float mouseSensitivity = 2f;
-
     public Transform playerCamera;
 
-    private float xRotation = 0f;
+    private Rigidbody rb;
+    private float xRotation;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        // For testing and debugging
-        Debug.Log("Player script is running");
+        LookAround();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 move = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
-            Debug.Log("W key is pressed");
-
-
-
-
-        // WASD movement, same style as your cannon project
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            move += transform.forward;
 
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            move -= transform.forward;
 
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            move -= transform.right;
 
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            move += transform.right;
 
-        // mouse look
+        Vector3 newPosition = rb.position + move.normalized * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+    void LookAround()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
@@ -46,6 +56,7 @@ public class MazePlayerController : MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
